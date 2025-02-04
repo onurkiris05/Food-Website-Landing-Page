6b8d960ed2a9d18b30a5f3ad7d57232012d1c923
onurkiris <83399747+onurkiris05@users.noreply.github.com>
2025-02-04 21:10:25 +0300

    [Added] Bar and Banner Content

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 const searchContainer = document.querySelector(".search-container");
 const searchLogo = document.querySelector(".search-logo");
+const btnLearnMore = document.querySelector(".btn-learn-more");
+const imgCard = document.querySelector(".img-card");
 
 searchLogo.addEventListener("click", () => {
   searchContainer.classList.toggle("active");
+});
+
+btnLearnMore.addEventListener("click", () => {
+  imgCard.classList.toggle("active");
 });
