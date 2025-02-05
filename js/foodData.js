e8da6f58366fcee4466038979c35a0b6bf615586
onurkiris <83399747+onurkiris05@users.noreply.github.com>
2025-02-05 14:59:04 +0300

    [Added] Food Slider and Content

---
+++ js/foodData.js
@@ -0,0 +1,14 @@
+export const foodData = [
+  {
+    id: 1,
+    img: "/assets/images/food-1.jpeg",
+  },
+  {
+    id: 2,
+    img: "/assets/images/food-2.jpeg",
+  },
+  {
+    id: 3,
+    img: "/assets/images/food-3.jpeg",
+  },
+];
--- js/main.js
+++ js/main.js
@@ -1,12 +1,45 @@
+import { foodData } from "./foodData.js";
 const searchContainer = document.querySelector(".search-container");
 const searchLogo = document.querySelector(".search-logo");
 const btnLearnMore = document.querySelector(".btn-learn-more");
-const imgCard = document.querySelector(".img-card");
+const foodCard = document.querySelector(".food-card");
+const selectionOptions = document.querySelectorAll(".options");
+const cardImage = document.querySelector(".food-img");
+const navMobile = document.querySelector(".nav-mobile");
+const menuMobile = document.querySelector(".menu-mobile");
 
+// Search bar toggle
 searchLogo.addEventListener("click", () => {
   searchContainer.classList.toggle("active");
 });
 
+// Learn more button toggle
 btnLearnMore.addEventListener("click", () => {
-  imgCard.classList.toggle("active");
+  foodCard.classList.toggle("active");
+  btnLearnMore.classList.toggle("active");
+
+  if (btnLearnMore.classList.contains("active")) {
+    btnLearnMore.innerHTML = "Show Less <ion-icon name='chevron-collapse-outline'></ion-icon>";
+  } else {
+    btnLearnMore.innerHTML = "Learn More <ion-icon name='search-circle-outline'></ion-icon>";
+  }
+});
+
+// Food card change
+function changeCard(card) {
+  const id = card.getAttribute("data-id");
+  const selectedFood = foodData.find((item) => item.id == id);
+  cardImage.src = selectedFood.img;
+}
+
+selectionOptions.forEach((option) => {
+  option.addEventListener("click", () => {
+    changeCard(option);
+  });
+});
+
+// Mobile menu toggle
+menuMobile.addEventListener("click", () => {
+  navMobile.classList.toggle("active");
+  menuMobile.classList.toggle("active");
 });
