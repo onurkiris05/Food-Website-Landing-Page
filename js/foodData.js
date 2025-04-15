2d3581308857a5bc7f1b4258e7e1c1148aba58d0
onurkiris <83399747+onurkiris05@users.noreply.github.com>
2025-04-15 14:53:47 +0300

    [Updated] Images

--- js/foodData.js
+++ js/foodData.js
@@ -1,14 +1,14 @@
 export const foodData = [
   {
     id: 1,
-    img: "/assets/images/food-1.jpeg",
+    img: "https://res.cloudinary.com/djzahz5ym/image/upload/v1744717790/aary8l7ywlt5abiub2m4.jpg",
   },
   {
     id: 2,
-    img: "/assets/images/food-2.jpeg",
+    img: "https://res.cloudinary.com/djzahz5ym/image/upload/v1744717789/gz7laapa18pccjvq97wz.jpg",
   },
   {
     id: 3,
-    img: "/assets/images/food-3.jpeg",
+    img: "https://res.cloudinary.com/djzahz5ym/image/upload/v1744717789/jocu4egr2va2ato9oqhc.jpg",
   },
 ];
